4e8284fb3a14c3900e8bea2623e6baadcb5915d4
Rajveer Rathore <rajveerr@gmail.com>
2017-02-20 12:07:25 -0500

    Adding Gulp Image

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,18 +6,39 @@ var gulp = require("gulp"),
 		bourbon = require("node-bourbon").includePaths,
 		neat = require("node-neat").includePaths,
 		refills = require("node-refills").includePaths,
-		jade = require('gulp-jade');
+		jade = require('gulp-jade'),
+		images = require('gulp-image');
 
 
-// Compiles all gulp tasks
-gulp.task("default", ["sass"]);
+gulp.task('images', function () {
+  gulp.src('app/images/*')
+    .pipe(images({
+			pngquant: false,
+      optipng: false,
+      zopflipng: false,
+      jpegRecompress: false,
+      jpegoptim: false,
+      mozjpeg: false,
+      gifsicle: false,
+      svgo: false,
+      concurrent: 10
+		}))
+    .pipe(gulp.dest('dist/images'));
+});
 
 // Live reload anytime a file changes
-gulp.task("watch", ["browserSync", "sass"], function() {
-	gulp.watch("src/scss/**/*.scss", ["sass"]);
+gulp.task("watch", ["browserSync", "sass", "images", "templates"], function() {
+	gulp.watch("app/scss/**/*.scss", ["sass"]);
 	gulp.watch("dist/*.html").on("change", browserSync.reload);
-	gulp.watch('src/*.jade',['templates']);
+	gulp.watch('app/*.jade',['templates']);
+	gulp.watch('app/images/*',['images']);
 });
+
+//default task that's not used?
+// gulp.task('default', ['templates', 'images'], function () {
+//     browserSync({server: './dist'});
+//     gulp.watch('./app/*.jade', ['templates']);
+// });
 
 // Spin up a server
 gulp.task("browserSync", function() {
@@ -30,7 +51,7 @@ gulp.task("browserSync", function() {
 
 // Compile SASS files
 gulp.task("sass", function() {
-	gulp.src("src/scss/**/*.scss")
+	gulp.src("app/scss/**/*.scss")
 			.pipe(sass({
 				includePaths: bourbon,
 				includePaths: neat,
@@ -44,14 +65,8 @@ gulp.task("sass", function() {
 
 // Compile jade template to html
 gulp.task('templates', function() {
-    return gulp.src('./src/*.jade')
+    return gulp.src('./app/*.jade')
         .pipe(jade())
         .pipe(gulp.dest('./dist/'))
-        .pipe(reload({stream: true}));
-});
-
-
-gulp.task('default', ['templates'], function () {
-    browserSync({server: './dist'});
-    gulp.watch('./src/*.jade', ['templates']);
+        // .pipe(reload({stream: true}));
 });
